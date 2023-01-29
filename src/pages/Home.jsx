91294490475cc7c60fd851829a31e4c7ee2b71e9
kamal <kamade123@gmail.com>
2023-01-29 13:57:55 +0100

    added featured product section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,6 +53,108 @@ function Home() {
        
       </div>
     </div>
+
+    <section className="featured">
+      <h2>Featured</h2>
+      {/* featuered  item start*/}
+      <div className="featured__item">
+        <img src="../../assets/Rectangle 299.png" alt="" />
+        <div className="featured__contents">
+          <h2>The Boolean Egyptian</h2>
+          <p>
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit ut aliquam, 
+            purus sit amet luctus venenatis, 
+            lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur
+          </p>
+          <div className="featured__creators">
+            <div className="creators__imgs">
+              <img src="../../assets/Ellipse 14.png" alt="" />
+              <img src="../../assets/Ellipse 15.png" alt=""  className='move' />
+              <img src="../../assets/Ellipse 16.png" alt="" className='move'/>
+              <img src="../../assets/Ellipse 17.png" alt="" className='move'/>
+              <img src="../../assets/Ellipse 18.png" alt="" className='move'/>
+            </div>
+
+            <p>64 major creators</p>
+            <div className="arrow">
+              <img src="../../assets/Vector (4).png" alt=""/>
+            </div>
+           
+            
+          </div>
+        </div>
+
+      </div>
+      {/* featuered  item end*/}
+
+      {/* featuered  item start*/}
+      <div className="featured__item reverse">
+        <img src="../../assets/Rectangle 299 (1).png" alt="" />
+        <div className="featured__contents">
+          <h2>The Boolean Egyptian</h2>
+          <p>
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit ut aliquam, 
+            purus sit amet luctus venenatis, 
+            lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur
+          </p>
+          <div className="featured__creators">
+            <div className="creators__imgs">
+              <img src="../../assets/Ellipse 14.png" alt="" />
+              <img src="../../assets/Ellipse 15.png" alt=""  className='move' />
+              <img src="../../assets/Ellipse 16.png" alt="" className='move'/>
+              <img src="../../assets/Ellipse 17.png" alt="" className='move'/>
+              <img src="../../assets/Ellipse 18.png" alt="" className='move'/>
+            </div>
+
+            <p>64 major creators</p>
+            <div className="arrow">
+              <img src="../../assets/Vector (4).png" alt=""/>
+            </div>
+           
+            
+          </div>
+        </div>
+
+      </div>
+      {/* featuered  item end*/}
+
+            {/* featuered  item start*/}
+      <div className="featured__item">
+        <img src="../../assets/Rectangle 299 (2).png" alt="" />
+        <div className="featured__contents">
+          <h2>The Boolean Egyptian</h2>
+          <p>
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit ut aliquam, 
+            purus sit amet luctus venenatis, 
+            lectus magna fringilla urna, porttitor 
+            rhoncus dolor pur
+          </p>
+          <div className="featured__creators">
+            <div className="creators__imgs">
+              <img src="../../assets/Ellipse 14.png" alt="" />
+              <img src="../../assets/Ellipse 15.png" alt=""  className='move' />
+              <img src="../../assets/Ellipse 16.png" alt="" className='move'/>
+              <img src="../../assets/Ellipse 17.png" alt="" className='move'/>
+              <img src="../../assets/Ellipse 18.png" alt="" className='move'/>
+            </div>
+
+            <p>64 major creators</p>
+            <div className="arrow">
+              <img src="../../assets/Vector (4).png" alt=""/>
+            </div>
+           
+            
+          </div>
+        </div>
+
+      </div>
+      {/* featuered  item end*/}
+    </section>
    </div>
   )
 }
