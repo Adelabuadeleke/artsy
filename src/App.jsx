88af8a1d412e3a5e47cd4513c805deb14e95d1b1
kamal <kamade123@gmail.com>
2023-06-22 21:38:32 +0100

    added routes to marketplace,auction & drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import Home from './pages/Home';
+import Marketplace from './pages/Marketplace';
+import Auctions from './pages/Auctions';
+import Drop from './pages/Drop';
+import ItemDetails from './pages/ItemDetails';
+
 import {
   Navigate,
   Route,
@@ -13,6 +18,10 @@ function App() {
        <Router>
         <Routes>
           <Route path ='/' element = {<Home />}/>
+          <Route path='/marketplace' element={<Marketplace/>}/>
+          <Route path='/marketplace/item' element={<ItemDetails />}/>
+          <Route path='/auctions' element={<Auctions/>}/>
+          <Route path='/drop' element={<Drop/>}/>
         </Routes>
       </Router>
     </div>
