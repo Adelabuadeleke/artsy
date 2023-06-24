ad701ba2a34870ddba96a5d257b1e1908cfcf3e6
kamal <kamade123@gmail.com>
2023-06-24 15:31:10 +0100

    added auction page

---
+++ src/pages/Auctions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+function Auctions() {
+ return (
+  <div className='auctions'>
+   <Nav />
+  </div>
+)
+}
+
+export default Auctions
