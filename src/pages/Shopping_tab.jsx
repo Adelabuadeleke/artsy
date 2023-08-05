636466437a1d930d7686ee1e7c378611e7b6800c
kamal <kamade123@gmail.com>
2023-08-05 22:42:28 +0100

    added shopping tab page

---
+++ src/pages/Shopping_tab.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+function Shopping_tab() {
+  return (
+    <div className='shopping_tab'>
+     <Nav />
+
+    </div>
+  )
+}
+
+export default Shopping_tab
