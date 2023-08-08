3f85a5221f0d231f9e91b464cc0335e21534694a
kamal <kamade123@gmail.com>
2023-08-08 23:19:02 +0100

    added Thank you page structure

--- src/pages/Thank_you.jsx
+++ src/pages/Thank_you.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Nav from '../components/Nav'
+import '../css/Thankyou.css'
 
 function Thank_you() {
   return (
     <div className='thank_you'>
-
+      <Nav />
+      <div className="thank_you_body">
+        <img src="../../assets/Thankyou/Woman get online delivery.png" alt="" />
+        <h2>Hey Celestina, thank you for your purchase. </h2>
+        <small>You are amazing. Cheers to being <span>ARTSY!</span> <img src="../../assets/Thankyou/Illustration.png" alt="" /> </small>
+      </div>
     </div>
   )
 }
