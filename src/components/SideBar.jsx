74b725b4efeafb9ec0f46c68976eaf77518b9adc
kamal <kamade123@gmail.com>
2023-09-11 00:58:08 +0100

    added sidebar top

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import '../css/SideBar.css';
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
 
 function SideBar() {
   return (
    <aside className="sidebar">
-    
+    <div className="sidebar_top">
+      <h2>artsy.</h2>
+      <ClearOutlinedIcon className='sidebar_cancel'/>
+    </div>
    </aside>
   )
 }
